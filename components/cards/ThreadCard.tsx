d2012c44c5584b66370cbd9f6e3df45442c506dc
Ivan <ivanjosevera28@gmail.com>
2024-01-15 12:38:29 -0500

    finished Thread Card structure

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -34,6 +34,7 @@ const ThreadCard = ({
   community,
   createdAt,
   comments,
+  isComment,
 }: Props) => {
   return (
     <article className="flex w-full flex-col rounded-xl bg-dark-4 p-7">
@@ -90,6 +91,13 @@ const ThreadCard = ({
                   className="cursor-pointer object-contain"
                 />
               </div>
+              {isComment && comments.length > 0 && (
+                <Link href={`/thread/${id}`}>
+                  <p className="mt-1 text-subtle-medium text-gray-1">
+                    {comments.length} replies
+                  </p>
+                </Link>
+              )}
             </div>
           </div>
         </div>
